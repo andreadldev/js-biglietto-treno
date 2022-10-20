33d2dafe89b84a25f98096eee99b8ee3403df6b9
andreadldev <andrea.delisi2@gmail.com>
2022-10-20 18:09:52 +0200

    Aggiungi calcoli sconto (visibile solo in console)

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const age = prompt("Inserire età passeggero");
 const km = prompt("Inserire numero di chilometri da percorrere")
 
+
 const price = km * 0.21
 
-console.log(price)
-
 if (age < 18) {
-    console.log("È previsto lo sconto per i passeggeri minorenni del 20% sul prezzo finale del biglietto. Buon viaggio!")
+    const discount = (price * 20) / 100;
+    const newPrice = price - discount;
+    console.log(`Costo del biglietto in base ai chilometri da percorrere: ${price}`);
+    console.log(`È previsto lo sconto per i passeggeri minorenni del 20% sul costo del biglietto. Il prezzo finale del biglietto è ${newPrice.toFixed(2)}`);
+    console.log("Buon viaggio!")
 }
 else if (age >= 65) {
-    console.log("È previsto lo sconto per i passeggeri over 65 del 40% sul prezzo finale del biglietto. Buon viaggio!")
+    const discount = (price * 40) / 100;
+    const newPrice = price - discount;
+    console.log(`Costo del biglietto in base ai chilometri da percorrere: ${price}`);
+    console.log(`È previsto lo sconto per i passeggeri over 65 del 40% sul costo del biglietto. Il prezzo finale del biglietto è ${newPrice.toFixed(2)}`);
+    console.log("Buon viaggio!")
 }
 else {
-    console.log("Buon viaggio!")
+    console.log(`Costo del biglietto in base ai chilometri da percorrere: ${price}`);
+    console.log("Buon viaggio!");
 }
